1902cd3104a963aa7337114b943b02f0749bf64f
Victor <Victor@DESKTOP-AAJDOL9>
2020-02-20 19:13:50 +0100

    Version 2.1

--- Google/src/book/Book.java
+++ Google/src/book/Book.java
@@ -3,9 +3,19 @@ package book;
 public class Book {
 	
 	int ID; //From 0 to total-1
+	int Rate;
 	
-	public Book(int ID) {
+	public Book(int ID, int Rate) {
 		this.ID = ID;
+		this.Rate = Rate;
+	}
+
+	public int getRate() {
+		return Rate;
+	}
+
+	public void setRate(int rate) {
+		Rate = rate;
 	}
 
 	public int getID() {
--- Google/src/library/Library.java
+++ Google/src/library/Library.java
@@ -39,6 +39,5 @@ public class Library {
 	}
 	
 	public static void main(String[] args) {
-	
 	}
 }
